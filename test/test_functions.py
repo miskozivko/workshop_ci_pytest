e4616c7db0fda515bd39f964fec8a18747d27e5f
miskozivko <58524880+miskozivko@users.noreply.github.com>
2022-06-22 05:41:33 -0400

    Parametrize our test function

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,4 +1,8 @@
 from functions import add
+import pytest
 
-def test_add():
-  assert add(3, 4) == 7
+@pytest.mark.parametrize(
+  "x, y, result", [(1,2,3), (3,4,7), (4,5,9)]
+)
+def test_add(x, y, result):
+  assert add(x, y) == result
